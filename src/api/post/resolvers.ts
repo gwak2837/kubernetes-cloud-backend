c1bac2817bdfba16c1aab4555b676f8635243557
Taeuk Gwak <gwak2837@kakao.com>
2021-11-02 19:00:10 +0900

    Update resonse structure

--- src/api/post/resolvers.ts
+++ src/api/post/resolvers.ts
@@ -13,11 +13,13 @@ export const postRouter = new Router<UserContext>({ prefix: __dirname.slice(7) }
 
 postRouter.get('/', async (ctx, next) => {
   const { offset, limit } = ctx.params
-  const { rowCount, rows } = await pool
-    .query(posts, [limit ?? 10, offset ?? 0])
-    .catch((error) => ctx.throw(500, 'Database ' + error))
+  const { rowCount, rows } = await pool.query(posts, [limit ?? 10, offset ?? 0]).catch((error) => {
+    console.error(error)
+    return ctx.throw(500, JSON.stringify({ error: { message: 'Database error' } }))
+  })
 
-  if (rowCount === 0) return ctx.throw(404, '게시글이 존재하지 않습니다.')
+  if (rowCount === 0)
+    return ctx.throw(404, JSON.stringify({ message: '게시글이 존재하지 않습니다.' }))
 
   ctx.body = rows.map((row) => postORM(row))
   return next()
@@ -27,9 +29,10 @@ postRouter.get('/:id', async (ctx, next) => {
   const postId = ctx.params.id
   const { rowCount, rows } = await pool
     .query(post, [postId])
-    .catch((error) => ctx.throw(500, 'Database ' + error))
+    .catch((error) => ctx.throw(500, JSON.stringify({ message: 'Database ' + error })))
 
-  if (rowCount === 0) return ctx.throw(404, '해당 ID의 게시글이 존재하지 않습니다.')
+  if (rowCount === 0)
+    return ctx.throw(404, JSON.stringify({ message: '해당 ID의 게시글이 존재하지 않습니다.' }))
 
   ctx.body = postORM(rows[0])
   return next()
@@ -37,14 +40,19 @@ postRouter.get('/:id', async (ctx, next) => {
 
 postRouter.post('/', async (ctx, next) => {
   const userId = ctx.state.userId
-  if (!userId) return ctx.throw(403, '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.')
+  if (!userId)
+    return ctx.throw(
+      403,
+      JSON.stringify({ message: '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.' })
+    )
 
   const { title, contents } = ctx.request.body
-  if (!title || !contents) return ctx.throw(400, '게시글 제목과 내용을 입력해주세요.')
+  if (!title || !contents)
+    return ctx.throw(400, JSON.stringify({ message: '게시글 제목과 내용을 입력해주세요.' }))
 
   const { rows } = await pool
     .query(insertPost, [title, contents, userId])
-    .catch((error) => ctx.throw(500, 'Database ' + error))
+    .catch((error) => ctx.throw(500, JSON.stringify({ message: 'Database ' + error })))
 
   ctx.body = { postId: rows[0].id }
   return next()
@@ -52,7 +60,11 @@ postRouter.post('/', async (ctx, next) => {
 
 postRouter.patch('/:id', async (ctx, next) => {
   const userId = ctx.state.userId
-  if (!userId) return ctx.throw(403, '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.')
+  if (!userId)
+    return ctx.throw(
+      403,
+      JSON.stringify({ message: '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.' })
+    )
 
   const postId = ctx.params.id
   ctx.body = updatePost + postId
@@ -61,14 +73,21 @@ postRouter.patch('/:id', async (ctx, next) => {
 
 postRouter.delete('/:id', async (ctx, next) => {
   const userId = ctx.state.userId
-  if (!userId) return ctx.throw(403, '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.')
+  if (!userId)
+    return ctx.throw(
+      403,
+      JSON.stringify({ message: '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.' })
+    )
 
   const postId = ctx.params.id
   const { rowCount, rows } = await pool
     .query(deletePost, [postId, userId])
-    .catch((error) => ctx.throw(500, 'Database ' + error))
+    .catch((error) => ctx.throw(500, JSON.stringify({ message: 'Database ' + error })))
   if (rowCount === 0)
-    return ctx.throw(404, '해당 ID의 게시글이 존재하지 않거나 본인의 게시물이 아닙니다.')
+    return ctx.throw(
+      404,
+      JSON.stringify({ message: '해당 ID의 게시글이 존재하지 않거나 본인의 게시물이 아닙니다.' })
+    )
 
   ctx.body = { postId: rows[0].id }
   return next()
