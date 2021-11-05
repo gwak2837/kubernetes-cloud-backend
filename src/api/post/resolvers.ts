dd5e21fb326144e1e8b2aefc1abb23059fecfdd2
Taeuk Gwak <gwak2837@kakao.com>
2021-11-05 14:27:24 +0900

    feat: add post patch api

--- src/api/post/resolvers.ts
+++ src/api/post/resolvers.ts
@@ -7,7 +7,7 @@ import deletePost from './sql/deletePost.sql'
 import insertPost from './sql/insertPost.sql'
 import post from './sql/post.sql'
 import posts from './sql/posts.sql'
-import updatePost from './sql/updatePost.sql'
+import updatePostBasic from './sql/updatePost.sql'
 
 export const postRouter = new Router<UserContext>({ prefix: __dirname.slice(7) })
 
@@ -63,11 +63,44 @@ postRouter.patch('/:id', async (ctx, next) => {
   if (!userId)
     return ctx.throw(
       403,
-      JSON.stringify({ message: '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.' })
+      JSON.stringify({
+        error: { message: '로그인되어 있지 않습니다. 로그인 후 다시 요청해주세요.' },
+      })
     )
 
   const postId = ctx.params.id
-  ctx.body = updatePost + postId
+  const { title, contents } = ctx.request.body
+  if (!title && !contents)
+    return ctx.throw(
+      400,
+      JSON.stringify({ error: { message: '게시글 제목과 내용을 입력해주세요.' } })
+    )
+
+  let insertingSql = ''
+
+  if (title) {
+    insertingSql += ', title = $3 '
+  }
+  if (contents) {
+    insertingSql += ', contents = $4 '
+  }
+
+  const insertingIndex = updatePostBasic.indexOf('WHERE')
+  const updatePost =
+    updatePostBasic.slice(0, insertingIndex) + insertingSql + updatePostBasic.slice(insertingIndex)
+
+  const { rowCount, rows } = await pool
+    .query(updatePost, [postId, userId, title, contents])
+    .catch((error) => ctx.throw(500, JSON.stringify({ error: { message: 'Database ' + error } })))
+  if (rowCount === 0)
+    return ctx.throw(
+      404,
+      JSON.stringify({
+        error: { message: '해당 ID의 게시글이 존재하지 않거나 본인의 게시물이 아닙니다.' },
+      })
+    )
+
+  ctx.body = rows[0]
   return next()
 })
 
--- src/api/post/sql/updatePost.sql
+++ src/api/post/sql/updatePost.sql
@@ -1,1 +1,5 @@
-UPDATE
+UPDATE post
+SET modification_time = CURRENT_TIMESTAMP
+WHERE id = $1
+  AND user_id = $2
+RETURNING *
