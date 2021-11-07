53612a79c90fba5601b984c43160185841e194a1
Taeuk Gwak <gwak2837@kakao.com>
2021-11-08 02:47:44 +0900

    Update postgres.ts

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -1,5 +1,7 @@
 import { Pool } from 'pg'
 
+console.log('👀 - process.env.CA_CERTIFICATE', process.env.CA_CERTIFICATE)
+
 export const pool = new Pool({
   connectionString: process.env.CONNECTION_STRING,
   ...(process.env.NODE_ENV === 'production' && {
